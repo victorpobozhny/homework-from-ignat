468e8177a27632d7b334932e862185ecc26ead2c
Victor <pobozhny94@gmail.com>
2023-12-11 16:20:08 +0300

    some logic, css

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -14,7 +14,17 @@ import avatar from './avatar.png'
 * */
 
 // нужно создать правильный тип вместо any
-export type MessageType = any
+export type MessageType = {
+    id: number
+    user: {
+        avatar: string
+        name: string
+    }
+    message: {
+        text: string
+        time: string
+    }
+}
 
 // структуру объекта не менять
 export const message0: MessageType = {
@@ -42,9 +52,9 @@ export const friendMessage0: MessageType = {
 
 const HW1 = () => {
     return (
-        <div id={'hw1'}>
-            <div className={s2.hwTitle}>Homework #1</div>
-            <div className={s2.hw}>
+        <div id={'hw1'} className={s2.hw1Wrapper}>
+            <div className={s2.hw1Title}>Hometask #1</div>
+            <div className={s2.hw1}>
                 {/*проверка отображения (не менять)*/}
                 <div>
                     <Message message={message0} />
